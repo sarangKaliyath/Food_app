0cda731809a57cc6b278a70b08e58699c02488c4
sarangKaliyath <86410007+sarangKaliyath@users.noreply.github.com>
2021-08-08 15:36:58 +0530

    first Page

---
+++ foodApp1/components/navbar.js
@@ -0,0 +1,30 @@
+
+function navbar() {
+    
+
+    return ` <div id="navbar">
+
+        <div class="container">
+
+            <input type="text" placeholder="Search Meals" id="searchBox">
+            <button >Search</button>
+
+            <div class="items border"></div>
+
+        </div>
+
+        
+        <div class="links">
+
+            <a href="">Receipe of the day</a>
+            <a href="">Latest Receipe</a>
+
+        </div>
+
+    </div>
+    
+    <div class="mainPage border";"></div>`;
+
+}
+
+export default navbar;
